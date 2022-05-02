15f0d6777d1ef7d0f4db615d6d8b66b97eae1c1e
zzay0921 <1015864587@qq.com>
2022-05-02 20:23:12 +0800

    update hystrix-dashboard-consumer-service

---
+++ cloud-hystrix-consumer-dashboard9001/src/main/java/com/github/zzay/HystrixDashboardMain9001.java
@@ -0,0 +1,21 @@
+package com.github.zzay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author zzay
+ * @className HystrixDashboardMain9001
+ * @description Hystrix Dashboard - 9001
+ * @create 2022/05/02 19:49
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+
+}
